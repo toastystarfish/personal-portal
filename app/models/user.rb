97f651c4b397c5e19c108107b5e5a33d0d8c8174
Jordan Johnson <jordan@tyemill.com>
2017-04-04 20:10:21 -0700

    commented the registerable option in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
 
   define_roles :developer, :owner, :admin
 
+  # WARNING: Enabling registerable will allow users to sign up for the
+  # application on their own.  This raises security requirements for the app.
+  # We prefer to use a invitation sign up model where administrators invite
+  # all new users to the application (This is enabled by the default app). 
+  #
+  # HERE BE DRAGONS.
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable,
