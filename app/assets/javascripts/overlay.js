6eb499e47e1065576352e2b83dfc2ee531c4432d
josh <josh@tyemill.com>
2018-06-13 12:52:32 -0700

    Add new default styles, reorganize and refactor CSS

---
+++ app/assets/javascripts/overlay.js
@@ -0,0 +1,28 @@
+
+// Any click on an overlay container will:
+// - close all open overlays
+// - open this overlay, if it is not already open
+$(document).on('click', '[data-overlay=\'overlay_container\']', function (e) {
+  var overlay_content = $(this).find('[data-overlay=\'overlay_content\']');
+  e.stopPropagation();
+  // Close all open overlays
+  overlay_fade_out($('[data-overlay=\'overlay_content\']:visible'));
+  // Close the overlay if visible; otherwise show it
+  if (!overlay_content.is(':visible')) {
+    overlay_content.show().addClass('anim-overlay-in');
+  }
+});
+
+// Clicking anywhere on the page body while an overlay is open should close it
+$(document).on('click', 'body', function (e) {
+  overlay_fade_out($('[data-overlay=\'overlay_content\']:visible'));
+});
+
+// Add class to animate out, and use event listener to hide the element once done
+function overlay_fade_out(overlay_content) {
+  overlay_content.addClass('anim-overlay-out').one('animationend', function() {
+    if (overlay_content.hasClass('anim-overlay-out')) {
+      overlay_content.removeClass('anim-overlay-out anim-overlay-in').hide();
+    }
+  });
+}
