9ff814415dcf00fbd0254b0b0f53201b7ffa6579
Jordan Johnson <johnson.jrdn@gmail.com>
2019-04-07 15:53:28 -0700

    generated home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :users
-  root 'users#index'
   devise_scope :user do
     get '/users/new/:token', controller: 'users', action: 'new',
-                       as: 'invited_new_user'
+    as: 'invited_new_user'
 
     get 'sign_in', to: 'devise/sessions#new'
     get 'sign_out', to: 'devise/sessions#destroy'
   end
 
   resources :invitations, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'home#show'
+
+  root 'home#show'
 end
